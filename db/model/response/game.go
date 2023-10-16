b05f1e2ebafc8360fdc1a50c2f44380813bd9dc4
jianglei <jianglei.new@gmail.com>
2023-10-16 16:41:27 +0800

    删掉没用到的结构体

--- db/model/response/game.go
+++ db/model/response/game.go
@@ -1,9 +1,7 @@
 package response
 
-import "gameday/db/model"
-
-type GameResponse struct {
-	GameName string `json:"game_name"`
-	//People   int                      `json:"people"`
-	HashList []*model.User `json:"hash_list"`
-}
+//type GameResponse struct {
+//	GameName string `json:"game_name"`
+//	//People   int                      `json:"people"`
+//	HashList []*model.User `json:"hash_list"`
+//}
